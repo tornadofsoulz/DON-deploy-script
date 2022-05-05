1623de6735db6ab1c5e4ffea655da5c7fd496ec6
Nick Tretyakov <tornadofsoulz@yandex.ru>
2022-05-06 01:26:35 +0300

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,94 @@
+#! /bin/bash
+
+echo '\033[0;35m
+  ██████  █    ██   ██████▓██   ██▓    ███▄    █  ▒█████  ▓█████▄ ▓█████ 
+▒██    ▒  ██  ▓██▒▒██    ▒ ▒██  ██▒    ██ ▀█   █ ▒██▒  ██▒▒██▀ ██▌▓█   ▀ 
+░ ▓██▄   ▓██  ▒██░░ ▓██▄    ▒██ ██░   ▓██  ▀█ ██▒▒██░  ██▒░██   █▌▒███   
+  ▒   ██▒▓▓█  ░██░  ▒   ██▒ ░ ▐██▓░   ▓██▒  ▐▌██▒▒██   ██░░▓█▄   ▌▒▓█  ▄ 
+▒██████▒▒▒▒█████▓ ▒██████▒▒ ░ ██▒▓░   ▒██░   ▓██░░ ████▓▒░░▒████▓ ░▒████▒
+▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░  ██▒▒▒    ░ ▒░   ▒ ▒ ░ ▒░▒░▒░  ▒▒▓  ▒ ░░ ▒░ ░
+░ ░▒  ░ ░░░▒░ ░ ░ ░ ░▒  ░ ░▓██ ░▒░    ░ ░░   ░ ▒░  ░ ▒ ▒░  ░ ▒  ▒  ░ ░  ░
+░  ░  ░   ░░░ ░ ░ ░  ░  ░  ▒ ▒ ░░        ░   ░ ░ ░ ░ ░ ▒   ░ ░  ░    ░   
+      ░     ░           ░  ░ ░                 ░     ░ ░     ░       ░  ░
+                           ░ ░                             ░'
+
+echo '\033[0m '
+echo "Welcome to Susy Chainlink Auditor installation guide!"
+echo "Press any key to resume ..."
+read temp
+
+sudo apt-get update
+
+docker -v
+if [ $? -eq 0 ]; then
+  echo '\033[0;33mdocker already installed'
+  echo '\033[0m '
+else 
+  # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  # echo 
+  # "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  # $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  # sudo apt-get update
+  # sudo apt-get install docker-ce docker-ce-cli containerd.io 
+  docker -v
+  if [ $? -eq 0 ]; then 
+    echo '\033[0;33mdocker successfully installed'
+    echo '\033[0m '
+    else
+    echo '\033[0;31mdocker installation aborted. Please check logs and install it manualy'
+    echo '\033[0m '
+    exit
+  fi
+fi
+
+docker-compose -v
+if [ $? -eq 0 ]; then
+  echo '\033[0;34mdocker-compose already installed'
+  echo '\033[0m '
+else 
+  sudo apt-get install docker-compose-plugin
+  docker-compose -v
+  if [ $? -eq 0 ]; then 
+    echo '\033[0;32mdocker-compose successfully installed'
+    echo '\033[0m '
+    else
+    echo '\033[0;31mdocker-compose installation aborted. Please check logs and install it manualy'
+    echo '\033[0m '
+    exit
+  fi
+fi
+
+cd /var/www && git clone https://github.com/SuSy-One/susy-chainlink-auditor
+echo '\033[0m '
+
+cd susy-chainlink-auditor/
+mkdir chainlink-1
+cd chainlink-1
+
+
+touch .password
+echo "Create new inner password(A password requires to have at least 3 numbers and 3 symbols):"
+read passw
+echo $passw > .password
+
+echo '\033[0m '
+
+touch .api
+echo "Enter your email:"
+read mail 
+echo $mail > .api
+
+echo "Create your password:"
+read password 
+echo $password >> .api
+
+  echo -e '\033[0;34mNode launch...' 
+  echo '\033[0m '
+
+
+cd /var/www/susy-chainlink-auditor && docker-compose up -d
+
+cd /var/www/susy-chainlink-auditor && docker-compose ps
+
+  echo '\033[0;33mUse http://localhost:7788 to access Chainlink Operator GUI if you are running the node on a local machine. If not - use ssh tunneling to be able to launch console using web browser: ssh -L 7788:localhost:7788 root@your.server.ip.address'
+  echo '\033[0m '
