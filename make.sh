fc9da60111a0e41c8994cf986aff6d7134fef693
Nick Tretyakov <tornadofsoulz@yandex.ru>
2022-05-06 01:35:22 +0300

    Fix docker installation

--- make.sh
+++ make.sh
@@ -24,12 +24,12 @@ if [ $? -eq 0 ]; then
   echo '\033[0;33mdocker already installed'
   echo '\033[0m '
 else 
-  # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  # echo 
-  # "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  # $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  # sudo apt-get update
-  # sudo apt-get install docker-ce docker-ce-cli containerd.io 
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo 
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io 
   docker -v
   if [ $? -eq 0 ]; then 
     echo '\033[0;33mdocker successfully installed'
